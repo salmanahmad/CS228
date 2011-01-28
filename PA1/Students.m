7afab0284cdc29b1b9117ba1a8fe400ad1f4a458
Salman Ahmad <salman@salmanahmad.com>
2011-01-27 17:58:09 -0800

    Readme and students

--- PA1/Students.m
+++ PA1/Students.m
@@ -1,6 +1,6 @@
 StudentInfo(1).fname = 'Salman';
 StudentInfo(1).lname = 'Ahmad';
-StudentInfo(1).ID = 1234567;  % Your SUID Number without the leading zeros
+StudentInfo(1).ID = 5573605;
 StudentInfo(2).fname = 'Andrew';
 StudentInfo(2).lname = 'Hershberger';
-StudentInfo(2).ID = 5585159;  % Partner's SUID Number without the leading zeros
+StudentInfo(2).ID = 5585159;
