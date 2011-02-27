f7e2536d80b730d02abe2e80843c9db1b7447296
Salman Ahmad <salman@salmanahmad.com>
2011-02-27 11:45:54 -0800

    Adding the ComputeLogLikelihood. It is now working. Log probabilities are evil

--- PA3/code/ComputeLogLikelihood.m
+++ PA3/code/ComputeLogLikelihood.m
@@ -8,10 +8,25 @@ function loglikelihood = ComputeLogLikelihood(logProb)
 N = size(logProb,1); % number of examples
 K = size(logProb,2); % number of classes
 
-loglikelihood = 0;
+loglikelihood = 1;
 
 % You should compute the log likelihood of data as in eq. (20) in the PA
 % description
 
 % YOUR CODE HERE
 
+for i = 1:N,
+    
+    x = 0;
+   for j = 1:K,
+       x = x + exp(logProb(i,j));
+   end;
+   
+    loglikelihood = loglikelihood * x;
+
+   
+end;
+
+loglikelihood = log(loglikelihood);
+
+i = 5;
