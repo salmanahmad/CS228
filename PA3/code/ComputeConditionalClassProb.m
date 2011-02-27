ba87e9e2022cd258a2e6bd925aa503342aee959d
Andrew Hershberger <andrew.d.hershberger@gmail.com>
2011-02-26 18:52:05 -0800

    ComputeConditionalClassProb.m works

--- PA3/code/ComputeConditionalClassProb.m
+++ PA3/code/ComputeConditionalClassProb.m
@@ -9,7 +9,18 @@ N = size(logProb,1); % number of examples
 K = size(logProb,2); % number of classes
 ClassProb = zeros(N,K);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE
+
+ClassProb = exp(logProb);
+
+% These lines just divide each row in ClassProb by the
+% sum of the row, producing rows normalized to one
+
+s = sum(ClassProb')';
+ClassProb = diag( 1 ./ s ) * ClassProb;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % ClassProb(i,k) should be the conditional probability of class label k
 % for example i in the dataset. (eq. (19) in PA description)
