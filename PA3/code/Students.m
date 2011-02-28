297ba4f95e84d2dcd491f218f894ccc5eadc4727
Salman Ahmad <salman@salmanahmad.com>
2011-02-28 15:45:24 -0800

    Adding the README and the Students.m

---
+++ PA3/code/Students.m
@@ -0,0 +1,6 @@
+StudentInfo(1).fname = 'Andrew';
+StudentInfo(1).lname = 'Hershberger';
+StudentInfo(1).ID = 5585159;
+StudentInfo(2).fname = 'Salman';
+StudentInfo(2).lname = 'Ahmad';
+StudentInfo(2).ID = 5573605;
